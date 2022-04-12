24d5454343ea7b1bcbb849f0b211c5b74549f04c
Lafrikana <102943752+Lafrikana@users.noreply.github.com>
2022-04-12 09:37:27 +0300

    Add cPanel and webmail redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/cpanel',
+        destination: 'https://host74.registrar-servers.com:2083',
+        permanent: true,
+      },
+      {
+        source: '/webmail',
+        destination: 'https://host74.registrar-servers.com:2096',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
