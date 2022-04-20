a54e6751a20e88e7743e3dfe1cb4969af5b37b29
Lafrikana <lafrikanaapp@gmail.com>
2022-04-20 17:42:14 +0300

    Compress member icon sizes | Update max-content width

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -54,7 +54,9 @@ export default function Layout({ children, ...pageProps }) {
 			<div id="container" className={ `env-${pageProps.env}` }>
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
 				<Menu env={ pageProps.env }/>
-				{children}
+				<div className="overflow-x-hidden">
+					{children}
+				</div>
 				<Footer/>
 			</div>
 		</>
